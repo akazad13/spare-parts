433d727912e5d3fda6a1dbb6837d0a1c17488172
Md. Abul Kalam <akazad.cse13@gmail.com>
2024-02-08 00:56:02 +0600

    Updated packages

--- spare-parts-web/src/app/pages/page-not-found/page-not-found.component.ts
+++ spare-parts-web/src/app/pages/page-not-found/page-not-found.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { UrlService } from '../../services/url.service';
 
 @Component({
-    selector: 'app-page-not-found',
-    templateUrl: './page-not-found.component.html',
-    styleUrls: ['./page-not-found.component.scss'],
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent {
-    constructor(
-        public url: UrlService,
-    ) { }
+  constructor(public url: UrlService) {}
 }
